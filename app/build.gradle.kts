ca7f38e95acbe611b1db99afaea3bce2fe47929c
Mohamed <raay77@gmail.com>
2026-03-30 12:52:28 +0300

    fix app build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,27 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.prayertime.watchface"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.prayertime.watchface"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
